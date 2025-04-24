38762e481713945fba35c1f64d086c0fe95cd188
Julien Atanassian <julien.atanassian@gmail.com>
2025-04-24 19:05:54 -0400

    add lecture skeleton

---
+++ m04w10_test-1-3-review/README.md
@@ -0,0 +1,7 @@
+# M04W10 - Test 1-3 Review
+
+[My latest recording](https://vimeo.com/879016755/2eeabd0e36?share=copy)
+
+- [ ] Programming Test 1 Review
+- [ ] Programming Test 2 Review
+- [ ] Programming Test 3 Review
---
+++ m04w10_test-1-3-review/test-1/q1.js
@@ -0,0 +1,54 @@
+/* Question 1
+ *
+ *  Implement the functions defined below
+ *
+ */
+
+/* ===========================================================================
+ * MIN - the lowest value in a list
+ *
+ * For example:
+ *
+ *    min([6,2,3,4,9,6,1,0,5])
+ *
+ * Returns:
+ *
+ *    0
+ */
+const min = function (arr) {
+  // Implement me
+};
+
+/* ===========================================================================
+ * MAX - the highest value in a list
+ *
+ * For example:
+ *
+ *    max([6,2,3,4,9,6,1,0,5])
+ *
+ * Returns:
+ *
+ *    9
+ */
+const max = function (arr) {
+  // Implement me
+};
+
+/* ===========================================================================
+ * RANGE - the difference between the highest and lowest values in a list
+ *
+ * For example:
+ *
+ *    range([6,2,3,4,9,6,1,0,5])
+ *
+ * Returns:
+ *
+ *    9
+ */
+const range = function (arr) {
+  // Implement me
+};
+
+// Don't change below:
+
+module.exports = { min, max, range };
